12a457028021b7c0c662eaa14e828ea88c65d499
ringga <jacksonstrom18@gmail.com>
2024-03-25 08:44:18 +0700

    utsringgamobile1

---
+++ app/src/main/java/utsringgamobile1/club.kt
@@ -0,0 +1,24 @@
+package utsringgamobile1
+
+// data class Club
+data class Club(
+    val name: String,
+    val epl: Int,
+    val fa: Int,
+    val efl: Int,
+    val ucl: Int,
+    val uel: Int,
+) {
+    val totalTrophy: Int
+        get() = epl + fa + efl + ucl +  uel
+}
+
+// extension function recap
+fun Club.recap(): String {
+    return "${this.name} berhasil meraih " +
+            "${this.epl} trofi Liga Primer Inggris, " +
+            "${this.fa} trofi FA, " +
+            "${this.efl} trofi EFL, " +
+            "${this.ucl} trofi Liga Champions, " +
+            "dan ${this.uel} trofi Liga Eropa UEFA"
+}
---
+++ app/src/main/java/utsringgamobile1/main.kt
@@ -0,0 +1,58 @@
+package utsringgamobile1
+fun main() {
+    // Inisialisasi mutableList dan tambah data List
+    val clubs = mutableListOf<Club>(
+        Club("Liverpool", 19, 8, 9, 6, 3),
+        Club("Manchester United", 20, 12, 6, 3, 1),
+        Club("Chelsea", 6, 6, 5, 2, 2),
+        Club("Manchester City", 8, 8, 8, 1, 0),
+        Club("Arsenal", 13, 14, 2, 0, 0)
+    )
+
+    // Passing a named function ::sortByNumberTrophyTotal
+    val sortedClubs = filterAndSort(clubs) {
+        sortByNumberTrophyTotal(it)
+    }
+    // Hasil sorting total trofi
+    println("Urutan klub berdasarkan jumlah total trofi: ")
+    for (club in sortedClubs) {
+        println("${club.name} - ${club.totalTrophy}")
+    }
+
+    // Passing a named function ::filterByEuropeanTrophy
+    val filteredClubs = filterAndSort(clubs) {
+        filterByEuropeanTrophy(it)
+    }
+    // Hasil sorting total trofi
+    println("Klub yang belum pernah memenangkan UCL dan UEL:")
+    for (club in filteredClubs) {
+        println("${club.name} - ${club.totalTrophy}")
+    }
+
+    // Panggil extension function Club recap
+    println("Rekap:")
+    for (club in clubs) {
+        println(club.recap())
+    }
+}
+
+// High order function filter and sort
+fun filterAndSort(clubs: List<Club>, options: (List<Club>) -> List<Club>): List<Club> {
+    return options(clubs)
+}
+
+// Fungsi sorting
+fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
+    val sortedClubs = clubs.sortedByDescending {
+        it.totalTrophy
+    }
+    return sortedClubs
+}
+
+// Fungsi filter
+fun filterByEuropeanTrophy(clubs: List<Club>): List<Club> {
+    val filteredClubs = clubs.filter {
+        it.ucl == 0 && it.uel == 0
+    }
+    return filteredClubs
+}
